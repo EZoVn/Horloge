b37f0a9aa9b03fd1f9da300a8027e328ce3b8f37
EzoVn <vincent.galpin@gmail.com>
2024-01-27 12:15:03 +0100

    Ajout de la date

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 let hrs = document.getElementById('hrs');
 let min = document.getElementById('min');
 let sec = document.getElementById('sec');
+let day = document.getElementById('day');
+let month = document.getElementById('month');
+let year = document.getElementById('year');
+let dayDate = document.getElementById('dayDate');
 
 setInterval(() => {
   let date = new Date();
-  // let hours = date.getHours();
   let hours = (date.getHours() < 10 ? '0' : '') + date.getHours();
   let minutes = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
   let seconds = (date.getSeconds() < 10 ? '0' : '') + date.getSeconds();
+
+  day.innerHTML = date.toLocaleDateString('en-US', { weekday: 'long' });
+  dayDate.innerHTML = date.getDate();
+  month.innerHTML = (date.getMonth() + 1 < 10 ? '0' : '') + (date.getMonth() + 1);
+  year.innerHTML = date.getFullYear();
 
   hrs.innerHTML = hours;
   min.innerHTML = minutes;
